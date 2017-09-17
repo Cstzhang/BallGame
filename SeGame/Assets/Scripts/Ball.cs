cdc6ac4f312717eb55b0055732cf3691ed141f5c
Cstzhang <835066041@qq.com>
2017-09-17 16:04:40 +0800

    add speed

--- SeGame/Assets/Scripts/Ball.cs
+++ SeGame/Assets/Scripts/Ball.cs
@@ -2,26 +2,30 @@
 using System.Collections;
 
 public class Ball : MonoBehaviour {
-	private Rigidbody2D rigidbody2D;
+	private Rigidbody2D rigidbodyBall;
 
-	// Use this for initialization
+	// start speed
 	void Start () {
-		rigidbody2D = GetComponent<Rigidbody2D> ();
+		rigidbodyBall = GetComponent<Rigidbody2D> ();
 		int number = Random.Range (0, 2);
-	
 		if (number == 1) {
-			rigidbody2D.AddForce (new Vector2 (100, 0));
-
-
+			rigidbodyBall.AddForce (new Vector2 (100, 0));
 		} else {
-			rigidbody2D.AddForce (new Vector2 (-100, 0));
+			rigidbodyBall.AddForce (new Vector2 (-100, 0));
 		
 		}
 	
 	}
-	
-	// Update is called once per frame
-	void Update () {
+	//碰撞事件
+	void OnCollisionEnter2D(Collision2D col) {
+		print ("Oncollision");
+		if (col.collider.tag == "Player") {
+			print ("velocity");
+			Vector2 velocity = rigidbodyBall.velocity;
+			velocity.y = velocity.y/2 + col.rigidbody.velocity.y/2;
+			rigidbodyBall.velocity = velocity;
+		}
 	
 	}
+
 }
